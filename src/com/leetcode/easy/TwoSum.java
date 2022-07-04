5766e2c8fa6e8eaf9acc36439bbdb795f2364a55
prabu766 <prabuddha.bhashitha@seera.sa>
2022-07-04 17:41:25 +0400

    Update TwoSum.java

--- src/com/leetcode/easy/TwoSum.java
+++ src/com/leetcode/easy/TwoSum.java
@@ -1,7 +1,5 @@
 package com.leetcode.easy;
 
-import java.util.Arrays;
-
 /**
  * Given an array of integers nums and an integer target, return indices of the
  * two numbers such that they add up to target. You may assume that each input
