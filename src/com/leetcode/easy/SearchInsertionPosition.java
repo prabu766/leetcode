a250491d02b68b58b667e02b406d5ff2fefc0778
prabu766 <prabuddha.bhashitha@seera.sa>
2023-02-20 16:16:48 +0400

    Adding the optimized solution

--- src/com/leetcode/easy/SearchInsertionPosition.java
+++ src/com/leetcode/easy/SearchInsertionPosition.java
@@ -45,27 +45,24 @@ public class SearchInsertionPosition {
 		return position;
 	}
 	
+	/**
+	 * This approach is better than the above as it make half the data set in each loop.
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
 	public static int searchInsertOptimized(int[] nums, int target) {
-		// if the target is greater than the array's max, return the next array index
-		if (target > nums[nums.length - 1]) {
-			return nums.length;
-		}
-		if (target > nums[0]) {
-			int start = 0;
-			int end =nums.length;
-			int position = start;
-			
-			while (position < end) {
-				if (target == nums[position]) {
-					return position;
-				}
-				if (target < nums[position + 1]) {
-					return position + 1;
-				}
-				++position;
-			}
-		}
-		return 0;
+		int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end-start)/2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) end = mid-1;
+            else start = mid + 1;
+        }
+
+        return start;
 	}
 
 }
