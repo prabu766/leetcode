6b28816dda6b2ad2d02a76504332f3ab6b00643f
prabu766 <prabuddha.bhashitha@seera.sa>
2023-03-02 17:35:44 +0400

    Update ContrainsDuplicate.java

--- src/com/leetcode/easy/ContrainsDuplicate.java
+++ src/com/leetcode/easy/ContrainsDuplicate.java
@@ -5,22 +5,39 @@ import java.util.Map;
 
 /**
  * 
+ * Given an integer array nums, return true if any value appears at least twice
+ * in the array, and return false if every element is distinct.
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [1,2,3,1] 
+ * Output: true 
+ * 
+ * Example 2: 
+ * Input: nums = [1,2,3,4] 
+ * Output: false 
+ * 
+ * Example 3:
+ * Input: nums = [1,1,1,3,3,4,3,2,4,2] 
+ * Output: true
+ * 
  * @author prabhuddha.bhashitha
  *
  */
 public class ContrainsDuplicate {
 
 	public static void main(String[] args) {
-		int[] intArr = {2,11,3,6,2};
+		int[] intArr = { 2, 11, 3, 6, 2 };
 		System.out.println(containsDuplicate(intArr));
 	}
+
 	public static boolean containsDuplicate(int[] nums) {
-        Map<Integer,Integer> countMap = new HashMap<>();
-        for(int i : nums){
-        	if(countMap.putIfAbsent(i, 1)!=null) {
-        		return true;
-        	}
-        }
-        return false;
-    }
+		Map<Integer, Integer> countMap = new HashMap<>();
+		for (int i : nums) {
+			if (countMap.putIfAbsent(i, 1) != null) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
