eae3f3dd90f2d4637b30997d2a0acf79b1a78451
prabu766 <prabuddha.bhashitha@almosafer.com>
2024-11-12 17:15:49 +0400

    Update RansomNote.java

--- src/com/leetcode/easy/RansomNote.java
+++ src/com/leetcode/easy/RansomNote.java
@@ -26,9 +26,30 @@ public class RansomNote {
 
 	public static void main(String[] args) {
 		
-		System.out.println(canConstruct("aa", "ab"));
+		System.out.println(canConstruct2("aa", "ab"));
 	}
 
+	
+	public static boolean canConstruct2(String ransomNote, String magazine) {
+        // Initialize an array of count with the size 26...
+        int[] counter = new int[128];
+        // Traverse a loop through the entire String of magazine where char ch stores the char at the index of magazine...
+        for (final char ch : magazine.toCharArray()) {
+            ++counter[ch];
+            
+            System.out.println("ch :"+ ch+ " "+ counter[ch]);
+            
+        }
+        
+        
+        // Run another for loop for ransomNote...
+        for (final char ch : ransomNote.toCharArray())
+             // If the charachter doesn't exists in magazine for ransomNote, we return false...
+            if (--counter[ch] < 0)
+                return false;
+        return true;      // If nothing goes wrong, return true...
+    }
+	
 	public static boolean canConstruct(String ransomNote, String magazine) {
 		if (ransomNote.length() > magazine.length()) {
 			return false;
